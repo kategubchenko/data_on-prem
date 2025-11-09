eba6b0b2e4e5b41dcc295637b94802c5341b738a
Kateryna Gubchenko <porotunya@gmail.com>
2025-11-09 21:54:32 +0000

    Update vcs.tf

--- vcs.tf
+++ vcs.tf
@@ -1,5 +1,6 @@
 resource "scalr_vcs_provider" "vcs" {
-  name     = "vcs_${formatdate("DDMMYYYY", timestamp())}"
+  name     = "vcs_upd${formatdate("DDMMYYYY", timestamp())}"
   vcs_type = var.base_vcs_type
   token    = var.base_vcs_token
 }
+
